5ba7d9bbfd49c28fd61ac31a74a4ed736a8feb3a
Jeffrey Adrion <jradrion@indiana.edu>
2020-05-05 23:58:14 +0000

    updated figures & added asiepel's suggestions

--- src/plot_HomSap_constant_size.R
+++ src/plot_HomSap_constant_size.R
@@ -4,8 +4,6 @@ library(cowplot)
 ## define variables
 infile="../data/HomSap_constant_size.txt"
 outfile="../display_items/HomSap_constant_size.pdf"
-species="Homo sapiens"
-pop0_name="Constant size"
 method1_name="SMC++"
 method2_name="stairway plot"
 method3_name="MSMC (n = 2)"
@@ -33,10 +31,9 @@ tab0A = subset(tab0,method == method1_id)
 tab0B = subset(tab0,method == method2_id)
 tab0C = subset(tab0,method == method3_id)
 tab0D = subset(tab0,method == method4_id)
-#tab0Y = subset(tab0,method == "census")
+tab0Y = subset(tab0,method == "census")
 tab0Z = subset(tab0,method == "coal")
 
-speciesPop <- bquote(italic(.(species)) ~ (.(pop0_name)))
 p0A<-ggplot(tab0A, aes(x=x, y=y, group=interaction(rep), colour=rep)) +
   coord_cartesian(xlim=c(xMin,xMax),ylim=c(yMin,yMax)) +
   geom_step(data=tab0Z,aes(x=x, y=y),size=3.3,alpha=1.0, col=truth_color) +
@@ -48,9 +45,9 @@ p0A<-ggplot(tab0A, aes(x=x, y=y, group=interaction(rep), colour=rep)) +
   scale_color_manual(values=replicate_colors) +
   guides(colour=guide_legend(keywidth=0.3,keyheight=0.21,default.unit="inch")) +
   theme_classic(base_size = 24) +
-  labs(x="", y="", title="", subtitle=method1_name, colour="", fill="")+
-  theme(axis.text.x = element_text(angle = 45,hjust = 0.5,vjust = 0.5)) 
-
+  labs(x="", y="", subtitle=method1_name, colour="", fill="")+
+  theme(axis.text.x = element_text(angle = 45,hjust = 0.5,vjust = 0.5),
+        plot.subtitle = element_text(size = 28)) 
 
 p0B<-ggplot(tab0B, aes(x=x, y=y, group=interaction(rep), colour=rep)) +
   coord_cartesian(xlim=c(xMin,xMax),ylim=c(yMin,yMax)) +
@@ -63,11 +60,11 @@ p0B<-ggplot(tab0B, aes(x=x, y=y, group=interaction(rep), colour=rep)) +
   scale_color_manual(values=replicate_colors) +
   guides(colour=guide_legend(keywidth=0.3,keyheight=0.21,default.unit="inch")) +
   theme_classic(base_size = 24) +
-  labs(x="", y="", title="", subtitle=method2_name) +
+  labs(x="", y="", subtitle=method2_name) +
   theme(axis.text.x = element_text(angle = 45,hjust = 0.5,vjust = 0.5)) +
   theme(axis.title.y=element_blank(),
         axis.text.y=element_blank(),
-        axis.ticks.y=element_blank())
+        plot.subtitle = element_text(size = 28))
 
 p0C<-ggplot(tab0C, aes(x=x, y=y, group=interaction(rep), colour=rep)) +
   coord_cartesian(xlim=c(xMin,xMax),ylim=c(yMin,yMax)) +
@@ -80,11 +77,11 @@ p0C<-ggplot(tab0C, aes(x=x, y=y, group=interaction(rep), colour=rep)) +
   scale_color_manual(values=replicate_colors) +
   guides(colour=guide_legend(keywidth=0.3,keyheight=0.21,default.unit="inch")) +
   theme_classic(base_size = 24) +
-  labs(x="", y="", title="", subtitle=method3_name) +
+  labs(x="", y="", subtitle=method3_name) +
   theme(axis.text.x = element_text(angle = 45,hjust = 0.5,vjust = 0.5))+
   theme(axis.title.y=element_blank(),
         axis.text.y=element_blank(),
-        axis.ticks.y=element_blank())
+        plot.subtitle = element_text(size = 28))
 
 p0D<-ggplot(tab0D, aes(x=x, y=y, group=interaction(rep), colour=rep)) +
   coord_cartesian(xlim=c(xMin,xMax),ylim=c(yMin,yMax)) +
@@ -97,15 +94,16 @@ p0D<-ggplot(tab0D, aes(x=x, y=y, group=interaction(rep), colour=rep)) +
   scale_color_manual(values=replicate_colors) +
   guides(colour=guide_legend(keywidth=0.3,keyheight=0.21,default.unit="inch")) +
   theme_classic(base_size = 24) +
-  labs(x="", y="", title="", subtitle=method4_name) +
+  labs(x="", y="", subtitle=method4_name) +
   theme(axis.text.x = element_text(angle = 45,hjust = 0.5,vjust = 0.5))+
   theme(axis.title.y=element_blank(),
         axis.text.y=element_blank(),
-        axis.ticks.y=element_blank())
+        axis.ticks.y=element_blank(),
+        plot.subtitle = element_text(size = 28))
 
 ###PLOTSAVE
 pg<-plot_grid(p0A, p0B, p0C, p0D, ncol=4, rel_widths = c(1.33,1,1,1))
-pg_custom<-ggdraw(add_sub(pg, expression(paste("Years ago")), vpadding=grid::unit(0,"lines"),y=6, x=0.5, vjust=5, size=30)) +
-draw_label("Population size", x=0, y=0.55, vjust= 1.5, angle=90, size=30)
+pg_custom<-ggdraw(add_sub(pg, expression(paste("Years ago")), vpadding=grid::unit(0,"lines"),y=6, x=0.54, vjust=5, size=28)) +
+draw_label(expression("Population size "*italic(N)*"(t)"), x=0, y=0.594, vjust= 1.25, angle=90, size=28)
 save_plot(outfile, pg_custom, base_height = 6 , base_width = 14)
 
